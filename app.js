8f5c480021aced244e4c879ed98bb7a1cbdb8eab
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-14 11:04:16 +0530

    Commit

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ console.log('\n\n-: App Started :-');
 
 const express   = require('express');
 const heapdump  = require('heapdump');
-heapdump.writeSnapshot('./my-snapshot.heapsnapshot');
+heapdump.writeSnapshot('./heapdump/my-snapshot.heapsnapshot');
 setInterval(() => { console.log(process.memoryUsage());}, 50000000);
 
 const app       = express();
+
+const dns = require('node:dns');
+
+dns.lookup('example.org', (err, address, family) => {
+  console.log('address: %j family: IPv%s', address, family);
+});
+// address: "2606:2800:21f:cb07:6820:80da:af6b:8b2c" family: IPv6
+
+
 
 app.use('/', (req, res, next)=>{
     console.log('-: Welcome :-');
