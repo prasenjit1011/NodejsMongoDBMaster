2161a0b7a97fe9bab9c67de249ca10c44fb352b4
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-16 08:47:40 +0530

    WebRTC

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 console.clear();
 console.log('\n\n-: App Started :-');
 
-const express   = require('express');
+const express   = require("express");
+const http      = require("http");
+const socketIo  = require("socket.io");
 const app       = express();
+const server    = http.createServer(app);
+const io        = socketIo(server);
 
-app.use('/', (req, res, next)=>{
-    console.log('-: Welcome :-');
-    res.send('-: Welcome :-');
-    next();
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("ice-candidate", (data) => socket.broadcast.emit("ice-candidate", data));
 });
 
 console.log('-: App Running :-');
-app.listen(3000);
+server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
