574bf12512aa0b0fe3d675e9fd8f8282e5a9f647
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-31 07:47:08 +0530

    Error Handling

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const app       = express();
 app.use('/', (req, res, next)=>{
     console.log('-: Welcome :-');
     res.send('-: Welcome :-');
-    next();
+
+    next()
+});
+
+// Centralized Error Handler
+app.use((err, req, res, next) => {
+    console.error('Central Error Handler:', err.message);
+    res.status(500).json({ message: 'Internal Server Error', error: err.message });
 });
 
 console.log('-: App Running :-');
