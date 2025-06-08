42ba4ec0c0a09519dd5cbd532a5341eb01c72b33
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-08 07:51:33 +0530

    Commit

---
+++ user-service/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  res.json([{ id: 1, name: 'Alice' }]);
+});
+
+app.listen(3001, () => console.log('User Service running on port 3001'));
