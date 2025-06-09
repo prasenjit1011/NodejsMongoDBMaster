2efb41f486b6059a3cd16eb5a21805ac18efcb24
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-09 20:18:32 +0530

    Commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
-
 const routes = require('./routes');
+
+app.use((req, res, next) => {
+    console.log('[Request] ::: ', req.method, req.url);  // ← log what Express sees
+    next();
+});
+
 app.use('/', routes);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.get('/about', (req, res) => {
-    res.send('About page from Lambda');
-});
-
-router.get('/dummy', (req, res) => {
-    res.send('Dummy page from Lambda');
-});
-
 router.get('/', (req, res) => {
     res.send('Welcome to Lambda Home Page!');
 });
   
+router.get('/dummy', (req, res) => {
+    res.send('Dummy page from Lambda');
+});
+
+router.get('/about', (req, res) => {
+    res.send('My About New page from Lambda');
+});
 
 module.exports = router;
