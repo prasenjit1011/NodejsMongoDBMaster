0400eb70beb9da00b9828f0a4fe756db911c3934
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-09 21:03:44 +0530

    Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome to Lambda Home Page! 2102');
+    res.send('Welcome to Lambda Home Page! 2102...');
 });
   
 router.get('/dummy', (req, res) => {
