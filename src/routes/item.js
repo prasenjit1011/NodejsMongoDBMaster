95d44a1891378713b5b869c0ab412945fb41927c
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-15 13:24:26 +0530

    C 1324

--- src/routes/item.js
+++ src/routes/item.js
@@ -5,8 +5,8 @@ const upload  = require('../middleware/upload');
 const itemCtrl = require('../modules/items/item.controller');
 const router  = express.Router();
 
-router.get('/', itemCtrl.getAllItem);
-router.post('/', auth, upload.array('images', 5), itemCtrl.createItem);
 
+router.get('/', itemCtrl.getAllItems);
+router.post('/', auth, upload.array('images', 5), itemCtrl.createItem);
 
 module.exports = router;
