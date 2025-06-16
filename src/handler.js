ef41eec619a548d65a2fd6796856615f1378b803
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-16 13:50:37 +0530

    commit 1350

--- src/handler.js
+++ src/handler.js
@@ -13,24 +13,3 @@ exports.handler = async (event, context) => {
   }
   return server(event, context);
 };
-
-
-
-// // handler.js
-// const serverless = require('serverless-http');
-// const app = require('./app');
-// const { connectToDatabase } = require('./mongo');
-
-// let isDbConnected = false;
-
-// const setup = async () => {
-//   if (!isDbConnected) {
-//     await connectToDatabase();
-//     isDbConnected = true;
-//   }
-// };
-
-// module.exports.handler = async (event, context) => {
-//   await setup();
-//   return serverless(app)(event, context);
-// };
